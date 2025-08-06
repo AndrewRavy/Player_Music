88041f1af201ae110d1f0ca5cafd36790e34766b
Andrew Ravy <andrewravy@gmail.com>
2025-08-06 15:34:54 -0300

    projeto teste

---
+++ script.js
@@ -0,0 +1,82 @@
+const songName = document.getElementById('song-name');
+const song = document.getElementById('audio');
+const bandName = document.getElementById('band-name');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+const aLittlePiece = {
+    songName : 'A Little Piece Of Heaven',
+    artist : 'Avenged Sevenfold',
+    file : 'a-little-piece-of-heaven'
+};
+
+const AndAllThings = {
+    songName : 'And All Things Will End',
+    artist : 'Avenged Sevenfold',
+    file : 'And-All-Things-Will-End'
+};
+
+let isPlaying = false;
+
+const playlist = [aLittlePiece,AndAllThings];
+let index = 0;
+
+function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong(){
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+function initializeSong(){
+    cover.src = `imagens/${playlist[index].file}.jpeg`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].artist;
+}
+
+function previousSong(){
+    if(index === 0){
+        index = playlist.length - 1;
+    }
+    else {
+        inde -= 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function nextSong(){
+    if(index === playlist.length - 1){
+        index = 0;
+    }
+    else {
+        inde += 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+initializeSong();
+play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+
